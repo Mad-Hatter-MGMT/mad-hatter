bcd267692012edf09cc738e951b9211cdc77c82b
tikisailor <40813246+tikisailor@users.noreply.github.com>
2022-12-21 17:34:28 -0700

    changelog

--- src/app/app.ts
+++ src/app/app.ts
@@ -52,11 +52,9 @@ function initializeClient(): Client {
 			Intents.FLAGS.GUILD_VOICE_STATES,
 			Intents.FLAGS.GUILD_PRESENCES,
 			Intents.FLAGS.GUILD_MESSAGES,
-			Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
 			Intents.FLAGS.DIRECT_MESSAGES,
-			Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
 		],
-		partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER'],
+		partials: ['MESSAGE', 'CHANNEL', 'USER'],
 	};
 	return new Discord.Client(clientOptions);
 }
